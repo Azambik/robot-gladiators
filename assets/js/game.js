29eba20587a9f4abd81b3ac12802205014164457
Azambik <71612017+Azambik@users.noreply.github.com>
2020-10-30 13:25:21 -0400

    added enemy array and logic to cycle through enemys

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,19 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 var playerName = window.prompt("what is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney =10;
 
 // You can also lo multiple values at once like thi console.log(playerName, playerAttack, playerHealth);
-var enemyName ="Roborto";
+var enemyNames = [ "Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack= 10;
 
-var fight = function() {
+var fight = function(enemyName) {
     //fight start anouncment
     window.alert("welcome to robot Gladiators!");
     //asking if you want to run or ight
@@ -59,5 +64,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
   }
 }
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
   
